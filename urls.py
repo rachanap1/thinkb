9f8981625176e29b56725fcec7f6d43476cb9dd2
rachanap1 <86865585+rachanap1@users.noreply.github.com>
2021-07-05 19:38:20 +0530

    C:\Users\Rachu\PycharmProjects\ShopBridge
    First commit

---
+++ urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.apiOverview,name = 'apiOverview'),
+    path('products/', views.index,name = 'index'),
+    path('product-list/', views.ShowAll,name = 'product-list'),
+    path('product-detail/<int:pk>/', views.ViewProduct,name = 'product-detail'),
+    path('product-create/', views.CreateProduct,name = 'product-create'),
+    path('product-update/<int:pk>/', views.updateProduct,name = 'product-update'),
+    path('product-delete/<int:pk>/', views.deleteProduct,name = 'product-delete'),
+]
---
+++ views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import ProductSerializer
+from .models import Product
+
+# Create your views here.
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html',{'products':products})
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+         'List': '/product-list/',
+         'Detail-view': '/product-detail/<int:id>',
+         'Create': '/product-create/',
+         'Update': '/product-update/<int:id>',
+         'Delete': '/product-delete/<int:id>',
+    }
+
+    return Response(api_urls);
+
+
+@api_view(['GET'])
+def ShowAll(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many = True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def ViewProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(product, many = False)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def CreateProduct(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def updateProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def deleteProduct(request, pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    return Response('Items deleted successfully!')
